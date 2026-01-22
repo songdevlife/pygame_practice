08da7c4a93f1388191ef20e3829a246f92b36046
Gonie <youcandoitgonie@gmail.com>
2026-01-22 18:14:49 +1030

    added Character and updated while loop

--- 3_main_sprite.py
+++ 3_main_sprite.py
@@ -11,6 +11,16 @@ screen = pygame.display.set_mode((screen_width,screen_height))
 # Background
 background = pygame.image.load("C:\\Users\\User\\Downloads\\game_project\\pygame_basic\\background.png")
 
+
+# Load Character 
+character = pygame.image.load("C:\\Users\\User\\Downloads\\game_project\\pygame_basic\\character.png")
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+character_x_pos = (screen_width / 2) - (character_width / 2)
+character_y_pos = screen_height - character_height
+
+
 # Game title
 pygame.display.set_caption("Gonie's Game")
 
@@ -25,7 +35,9 @@ while running:
             running = False
 
     screen.blit(background,(0,0))
+    
+    screen.blit(character,(character_x_pos,character_y_pos))
+    
     pygame.display.update()
-
 # End Game
 pygame.quit()
